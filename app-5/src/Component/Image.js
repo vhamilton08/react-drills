9064daf3c6d9427ed4c18c736ce827492464d0e0
vhamilton08 <vhamilton08@gmail.com>
2020-07-26 17:29:46 -0500

    afternoon work

---
+++ app-5/src/Component/Image.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Image extends Component {
+    render() {
+        return (
+            <div>
+                <img src={this.props.url} />
+            </div>
+        )
+    }
+}
